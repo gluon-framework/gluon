d605f783af52adbcb8f828e3af1c1079d9039028
CanadaHonk <oj@oojmed.com>
2022-12-09 18:00:55 +0000

    gluworld: rewrite into cool app

--- gluworld/index.js
+++ gluworld/index.js
@@ -6,4 +6,17 @@ import { join, dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-Gluon.open(pathToFileURL(join(__dirname, 'index.html')).href);
+Gluon.open(pathToFileURL(join(__dirname, 'index.html')).href, () => {
+  const setVersions = () => {
+    if (typeof chromium_version === 'undefined') return setTimeout(setVersions, 100);
+
+    chromium_version.textContent = CHROMIUM_VERSION;
+    node_version.textContent = NODE_VERSION;
+    gluon_version.textContent = GLUON_VERSION;
+    glugun_version.textContent = 'GLUGUN_VERSION' === 'G\LUGUN_VERSION' ? 'nothing' : 'Glugun GLUGUN_VERSION';
+  };
+
+  setVersions();
+}, {
+  windowSize: [ 800, 450 ]
+});
