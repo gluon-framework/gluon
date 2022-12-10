d873e8cc84e0ab01dbf60e7b513033c513223fda
CanadaHonk <oj@oojmed.com>
2022-12-10 01:28:20 +0000

    gluworld: rewrite to use new Gluon window object

--- gluworld/index.js
+++ gluworld/index.js
@@ -6,17 +6,6 @@ import { join, dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-Gluon.open(pathToFileURL(join(__dirname, 'index.html')).href, () => {
-  const setVersions = () => {
-    if (typeof chromium_version === 'undefined') return setTimeout(setVersions, 100);
-
-    chromium_version.textContent = CHROMIUM_VERSION;
-    node_version.textContent = NODE_VERSION;
-    gluon_version.textContent = GLUON_VERSION;
-    glugun_version.textContent = 'GLUGUN_VERSION' === 'G\LUGUN_VERSION' ? 'nothing' : 'Glugun GLUGUN_VERSION';
-  };
-
-  setVersions();
-}, {
+const Chromium = await Gluon.open(pathToFileURL(join(__dirname, 'index.html')).href, undefined, {
   windowSize: [ 800, 450 ]
 });
