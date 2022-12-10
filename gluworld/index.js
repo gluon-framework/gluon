a13c1d3196967577cee70bee184d96c830994d09
CanadaHonk <oj@oojmed.com>
2022-12-10 01:31:25 +0000

    gluworld: don't use top level await

--- gluworld/index.js
+++ gluworld/index.js
@@ -6,6 +6,8 @@ import { join, dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const Chromium = await Gluon.open(pathToFileURL(join(__dirname, 'index.html')).href, {
-  windowSize: [ 800, 450 ]
-});
+(async () => {
+  const Chromium = await Gluon.open(pathToFileURL(join(__dirname, 'index.html')).href, {
+    windowSize: [ 800, 450 ]
+  });
+})();
