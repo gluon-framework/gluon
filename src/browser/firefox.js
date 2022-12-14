4d9d264fa3466234224000d6899013b39a6bf151
Beef <beefers@riseup.net>
2022-12-14 18:23:47 +0000

    darwin: preliminary support

--- src/browser/firefox.js
+++ src/browser/firefox.js
@@ -18,6 +18,7 @@ user_pref('privacy.window.maxInnerHeight', ${windowSize[1]});`}
 user_pref('privacy.resistFingerprinting', true);
 user_pref('fission.bfcacheInParent', false);
 user_pref('fission.webContentIsolationStrategy', 0);
+${process.platform === 'darwin' ? `user_pref('browser.tabs.inTitlebar', 0);` : `` }
 `);
 
 // user_pref('privacy.resistFingerprinting', false);
