a9a1bd80b882746233e0c270cc55cebdc5fc4e2c
cijiugechu <cijiugechu@gmail.com>
2023-01-18 21:34:12 +0800

    fix: get port

--- src/launcher/start.js
+++ src/launcher/start.js
@@ -1,14 +1,12 @@
 import { spawn } from 'child_process';
+import getPort from 'get-port';
 import { log } from '../lib/logger.js';
 
 import ConnectCDP from '../lib/cdp.js';
 import InjectInto from './inject.js';
 
-const portRange = [ 10000, 60000 ];
-const generatePort = () => (Math.floor(Math.random() * (portRange[1] - portRange[0] + 1)) + portRange[0]);
-
 export default async (browserPath, args, transport, extra) => {
-  const port = transport === 'websocket' ? generatePort() : null;
+  const port = transport === 'websocket' ? await getPort({port: 10000}) : null;
 
   const proc = spawn(browserPath, [
     transport === 'stdio' ? `--remote-debugging-pipe` : `--remote-debugging-port=${port}`,
