0d3590b96f9c908acf5e4d9840dc51805e7e8ab1
CanadaHonk <oj@oojmed.com>
2023-01-28 12:13:59 +0000

    launcher: cleanup internally once process has exited

--- src/launcher/inject.js
+++ src/launcher/inject.js
@@ -40,6 +40,11 @@ export default async (CDP, proc, injectionType = 'browser', { dataPath, browserN
     }
   });
 
+  // when the process has exited (all windows closed), clean up window internally
+  proc.on('exit', () => {
+    Window.close();
+  });
+
   const browserInfo = await CDP.sendMessage('Browser.getVersion');
   log('browser:', browserInfo.product);
 
